cabebcdf96d18f0dfb9aaf668e0464a6f82f8be8
Kardin <oleynik624@gmail.com>
2023-06-20 23:20:28 +0600

    605. Can Place Flowers

---
+++ src/CanPlaceFlowers605.java
@@ -0,0 +1,26 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class CanPlaceFlowers605 {
+    private static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if (n == 0) {
+            return true;
+        }
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.length-1 || flowerbed[i+1] == 0)) {
+                flowerbed[i] = 1;
+                n--;
+                if (n == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] flowerbed = new int[]{1,0,0,0,1};
+        int n = 1;
+        System.out.print(canPlaceFlowers(flowerbed, n));
+    }
+}

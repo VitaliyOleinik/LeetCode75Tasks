5bfe91c7c59c73f1893e7c4c67cb49495f65b2ee
Kardin <oleynik624@gmail.com>
2023-06-24 10:15:40 +0600

    151. Reverse Words in a String

--- src/ReverseWordsInAString151.java
+++ src/ReverseWordsInAString151.java
@@ -1,22 +1,33 @@
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 public class ReverseWordsInAString151 {
     public static String reverseWords(String s) {
-        s = s.trim();
         boolean needSpace = true;
-        char[] array = s.toCharArray();
+        char[] array = s.trim().toCharArray();
+        Stack<String> filo = new Stack<>();
         StringBuilder arr = new StringBuilder();
-        for (int i = array.length - 1; i >= 0; i --) {
-            if ((array[i] >= 65 && array[i] <= 90) || (array[i] >= 97 && array[i] <= 122)) {
+        for (int i = 0; i <= array.length - 1; i++) {
+            if ((array[i] >= 65 && array[i] <= 90) || (array[i] >= 97 && array[i] <= 122)
+                    || (array[i] >= 48 && array[i] <= 57)) {
                 arr.append(array[i]);
                 needSpace = true;
-            } else if (array[i] == 32 && needSpace) {
-                arr.append(" ");
+            }
+            if ((array[i] == 32 || i == array.length - 1) && needSpace) {
+                filo.push(arr.toString());
+                arr = new StringBuilder();
                 needSpace = false;
             }
         }
-        return arr.toString();
+        StringBuilder result = new StringBuilder();
+        while (!filo.empty()) {
+            result.append(filo.peek()).append(" ");
+            filo.pop();
+        }
+        return result.toString().trim();
     }
 
     public static void main(String[] args) {
-        System.out.println(reverseWords("the sky is blue"));
+        System.out.println(reverseWords("the      sky     is   blue"));
     }
 }

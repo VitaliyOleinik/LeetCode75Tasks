95d6e81040e7386c63e8843fa8831c1886bf1ff5
VitaliyOleinik <oleynik624@gmail.com>
2023-07-04 21:25:52 +0600

    443. String Compression

--- src/StringCompression443.java
+++ src/StringCompression443.java
@@ -1,22 +1,30 @@
 public class StringCompression443 {
 
-    private static String compress(char[] chars) {
-        // test
-        int cnt = 0;
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < chars.length; i++) {
-            if (i+1 < chars.length && chars[i] == chars[i + 1]) {
-                cnt++;
-            } else if (i+1 < chars.length && cnt != 0 && chars[i] == chars[i + 1]) {
-                sb.append(chars[i]);
-                cnt = 0;
+    private static int compress(char[] chars) {
+        int j = 0, count = 1;
+
+        for(int i = 1; i < chars.length + 1; ++i) {
+            if(i == chars.length || chars[i] != chars[i - 1]) {
+                chars[j++] = chars[i - 1];
+
+                if(count > 1) {
+                    String s = count + "";
+
+                    for(int k = 0; k < s.length(); ++k)
+                        chars[j++] = s.charAt(k);
+                }
+
+                count = 1;
+            } else {
+                count++;
             }
         }
-        return sb.toString();
+
+        return j;
     }
 
     public static void main(String[] args) {
-        char[] chars = {'a','a','b','b','c','c','c'};
+        char[] chars = {'a','a','b','b','c','c','c', 'd'};
         System.out.println(compress(chars));
     }
 }

435a0563dc6c96f88c34ce3cb0b40cefa1534666
VitaliyOleinik <oleynik624@gmail.com>
2023-07-05 16:04:43 +0600

    283. Move Zeroes

--- src/MoveZeroes283.java
+++ src/MoveZeroes283.java
@@ -3,8 +3,8 @@ import java.util.Arrays;
 public class MoveZeroes283 {
 
     private static void moveZeroes(int[] nums) {
-        for (int i = nums.length - 1; i > 1 ; i--) {
-            for (int j = 0; j < nums.length - 1; j++) {
+        for (int i = nums.length - 1; i >= 1 ; i--) {
+            for (int j = 0; j < i; j++) {
                 if (nums[j] == 0) {
                     nums[j] = nums[j + 1];
                     nums[j + 1] = 0;
@@ -14,7 +14,7 @@ public class MoveZeroes283 {
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{0, 1, 0, 3, 12};
+        int[] nums = new int[]{0, 1};
         moveZeroes(nums);
         Arrays.stream(nums).forEach(System.out::println);
     }

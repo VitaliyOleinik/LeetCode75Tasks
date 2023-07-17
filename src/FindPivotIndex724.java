8dd43f5221822031c945e526f3c4295b1f942270
VitaliyOleinik <oleynik624@gmail.com>
2023-07-17 22:22:03 +0600

    724. Find Pivot Index

---
+++ src/FindPivotIndex724.java
@@ -0,0 +1,17 @@
+public class FindPivotIndex724 {
+
+    private static int pivotIndex(int[] nums) {
+        int sum = 0, leftsum = 0;
+        for (int x : nums) sum += x;
+        for (int i = 0; i < nums.length; i++) {
+            if (leftsum == sum - leftsum - nums[i]) return i;
+            leftsum += nums[i];
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 7, 3, 6, 5, 6};
+        System.out.println(pivotIndex(nums));
+    }
+}

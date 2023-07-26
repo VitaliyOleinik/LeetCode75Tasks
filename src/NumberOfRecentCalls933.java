348c3b2054974e6842777b22757014e5c6d725cd
VitaliyOleinik <oleynik624@gmail.com>
2023-07-26 23:39:58 +0600

    933. Number of Recent Calls

---
+++ src/NumberOfRecentCalls933.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+
+public class NumberOfRecentCalls933 {
+    LinkedList<Integer> slideWindow;
+
+    public RecentCounter() {
+        this.slideWindow = new LinkedList<Integer>();
+    }
+
+    public int ping(int t) {
+        // step 1). append the current call
+        this.slideWindow.addLast(t);
+
+        // step 2). invalidate the outdated pings
+        while (this.slideWindow.getFirst() < t - 3000)
+            this.slideWindow.removeFirst();
+
+        return this.slideWindow.size();
+    }
+}
